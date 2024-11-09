7d3736d19e8ee37db1ae5aafeb768ab90ba9b8db
mizan21 <mizanorrahman398@gmail.com>
2024-11-09 12:14:33 +0600

    Partnership part dynamic get

--- app/partnership/page.js
+++ app/partnership/page.js
@@ -1,10 +1,17 @@
 import PartnerSection from "@/components/Partnership/PartnerSection";
 import React from "react";
 
-const Partnership = () => {
+const Partnership = async () => {
+  const res = await fetch(
+    "https://living-brands-admin.vercel.app/api/partnership",
+    { next: { revalidate: 10 } }
+  );
+
+  const partnershipData = await res.json();
+
   return (
     <div>
-      <PartnerSection />
+      <PartnerSection partnersData={partnershipData} />{" "}
     </div>
   );
 };
--- components/Partnership/PartnerSection.jsx
+++ components/Partnership/PartnerSection.jsx
@@ -1,45 +1,32 @@
-"use client";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
-const PartnerSection = () => {
-  const [partnersData, setPartnersData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://living-brands-admin.vercel.app/api/partnership"
-        );
-        console.log(response);
-        setPartnersData(response.data);
-      } catch (error) {
-        console.error("Error fetching partner data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+const PartnerSection = ({ partnersData }) => {
   return (
-    <div className="bg-white py-10 relative z-[110] rounded-b-[20px] lg:rounded-b-[50px] font-sora">
+    <div className="bg-white py-10 relative z-[110] rounded-b-[20px] lg:rounded-b-[40px] font-sora">
       <div className="p-[5%]">
-        <h2 className="text-2xl md:text-3xl lg:text-[48px] leading-[1.2] font-bold text-[#125b5c] mb-10 tracking-tighter">
+        <h2
+          className="text-2xl md:text-3xl lg:text-[48px] font-bold text-[#125b5c] mb-10"
+          style={{ lineHeight: "1.03" }}
+        >
           Proudly Collaborating With The Most Cutting-Edge Platforms In The
           Industry
         </h2>
 
         <div className="grid grid-cols-1 lg:grid-cols-3  lg:gap-12 items-center text-center  justify-center mt-36">
-          {partnersData.map((partner, index) => (
+          {partnersData?.map((partner, index) => (
             <div key={index} className="text-center">
-              <img
-                src={partner.logo}
-                alt={`${partner.name} Logo`}
+              <Image
+                width={200}
+                height={200}
+                src={partner?.logo}
+                alt={`${partner?.name} Logo`}
                 className="mx-auto mb-2 h-16 object-contain saturate-0 hover:saturate-100"
               />
               <h3 className="text-[20px] tracking-tighter font-bold text-black mb-2 mt-10">
-                {partner.name}
+                {partner?.name}
               </h3>
               <p className="text-black opacity-75 text-[15px] font-[400] mb-2">
-                {partner.description}
+                {partner?.description}
               </p>
             </div>
           ))}
