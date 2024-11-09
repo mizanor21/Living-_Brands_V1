f898350a784ab18d433d5f6c97d99766a6fbb844
mizan21 <mizanorrahman398@gmail.com>
2024-11-09 11:17:27 +0600

    Award part polishing

--- app/achievements/page.js
+++ app/achievements/page.js
@@ -1,88 +1,74 @@
-"use client";
-import React, { useEffect, useState } from "react";
 import ButtonEffect from "../button/page";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 import Link from "next/link";
-import axios from "axios";
+import Image from "next/image";
 
-const Achievement = () => {
-  const [awards, setAwards] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchAwards = async () => {
-      try {
-        const response = await axios.get(
-          "https://living-brands-admin.vercel.app/api/achievements"
-        );
-        setAwards(response.data || []);
-      } catch (error) {
-        console.error("Failed to load awards data", error);
-        setError("Failed to load awards data.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAwards();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="text-center py-20">
-        <p>Loading achievements...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="text-center py-20 text-red-500">
-        <p>{error}</p>
-      </div>
-    );
-  }
+const Achievement = async () => {
+  const res = await fetch(
+    "https://living-brands-admin.vercel.app/api/achievements",
+    { next: { revalidate: 10 } }
+  );
+  const awards = await res.json();
 
   return (
-    <div className="bg-white relative z-[110] grid grid-cols-1 lg:gap-y-20 py-10 lg:py-20 font-sora rounded-b-[20px] lg:rounded-b-[50px]">
+    <div className="bg-white relative z-[110] py-10 lg:py-20 font-sora">
       {awards.map((award, index) => (
         <div
           key={award._id}
-          className={`grid gap-5 mb-8 sm:grid-cols-1 md:grid-cols-2 px-[5%] ${
-            index % 2 === 0 ? "" : "md:flex-row-reverse"
-          }`}
+          className="grid gap-8 mb-20 px-[5%] lg:gap-20 md:grid-cols-2"
         >
-          <div className="order-first md:order-last flex justify-center items-center">
+          {/* Image Part */}
+          <div
+            className={`flex justify-center items-center ${
+              index % 2 === 0 ? "order-2" : "order-1"
+            }`}
+          >
             {award.image ? (
-              <img src={award.image} alt={award.title} className="rounded-lg" />
+              <Image
+                width={900}
+                height={600}
+                src={award.image}
+                alt={award.title}
+                className="rounded-lg max-h-[800px]"
+              />
             ) : (
               <div className="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center text-gray-500">
                 No Image Available
               </div>
             )}
           </div>
-          <div className="order-last md:order-first">
-            <h1 className="text-2xl font-bold mt-1 md:text-3Xl lg:text-[48px] text-[#125b5c] mb-8">
+          {/* Content Part */}
+          <div
+            className={`flex flex-col justify-center ${
+              index % 2 === 0 ? "order-1" : "order-2"
+            }`}
+          >
+            <h1
+              className="text-2xl font-bold mt-1 md:text-3xl lg:text-[48px] text-[#125b5c] mb-8"
+              style={{ lineHeight: "1.03" }}
+            >
               {award.title}
             </h1>
             {award.description &&
               award.description.map((paragraph, i) => (
-                <p key={i} className="text-[18px] text-justify mb-4">
+                <p
+                  key={i}
+                  className="text-[18px] text-justify mb-2 leading-relaxed"
+                >
                   {paragraph}
                 </p>
               ))}
-            {award.link && (
-              <Link href={award.link}>
-                <div className="max-w-sm mt-10">
-                  <ButtonEffect>
-                    <span className="flex gap-2">
-                      Learn More <MdOutlineArrowRightAlt className="text-xl" />
-                    </span>
-                  </ButtonEffect>
-                </div>
-              </Link>
-            )}
+
+            <Link href={award?.link}>
+              <div className="max-w-sm mt-8">
+                <ButtonEffect>
+                  <span className="flex gap-2 items-center">
+                    Access Living Brand&apos;s Bots
+                    <MdOutlineArrowRightAlt className="text-xl" />
+                  </span>
+                </ButtonEffect>
+              </div>
+            </Link>
           </div>
         </div>
       ))}
