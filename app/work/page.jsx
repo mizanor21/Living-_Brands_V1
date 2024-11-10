a1fa40d8dcd60d67b12c5d9ca11eb05b2400206b
mizan21 <mizanorrahman398@gmail.com>
2024-11-11 04:34:09 +0600

    work page organize

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import Work from "../../components/Work/Work";
+import ButtonEffect from "../button/page";
+import Items from "@/components/Items/Items";
 
 const Works = async () => {
   return (
-    <>
-      <Work />
-    </>
+    <div className="bg-white relative z-[110] rounded-b-[40px] pb-10 lg:pb-20 font-sora">
+      <div className="px-[5%] pb-10">
+        <h2 className="text-2xl md:text-4xl lg:text-[48px] leading-10 text-[#125b5c] font-bold py-10 lg:pt-[80px] lg:pb-[70px]">
+          Explore Our latest Work
+        </h2>
+        <hr />
+        <div className="md:flex justify-end my-5 space-y-1 md:space-y-0">
+          <ButtonEffect>Casestudy</ButtonEffect>
+          <span className="divider divider-horizontal divider-start divider-neutral"></span>
+          <ButtonEffect>Daily Creativity</ButtonEffect>
+        </div>
+
+        <Items />
+      </div>
+    </div>
   );
 };
 
