6b0575dc56b94ff44a08ff016073d6a415eb7380
mizan21 <mizanorrahman398@gmail.com>
2024-12-09 14:36:36 +0600

    We Work part polishing

--- app/we-works/page.js
+++ app/we-works/page.js
@@ -6,6 +6,9 @@ const WeWork = () => {
     <div className="relative bg-white z-[110] rounded-b-[20px] lg:rounded-b-[60px]">
       <div className="px-[5%] ">
         <HowWeWorks></HowWeWorks>
+        <p className="font-sora font-[700] lg:text-[24px] py-10">
+          Our Principles
+        </p>
         <ColorPalette />
       </div>
     </div>
