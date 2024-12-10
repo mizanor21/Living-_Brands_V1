6cd326272d18d8e600cce6e98d55ae85aa18d63f
mizan21 <mizanorrahman398@gmail.com>
2024-12-10 17:42:04 +0600

    Popup part polishing

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Popup = () => {
   return (
     <div className="font-sora absolute flex justify-center items-center w-full z-[200] py-2">
-      <p className="font-[400] text-[18px]">
+      <p className="font-[400] text-[18px] lg:text-[18px]">
         &quot;Addikt is now part of the Schbang Network.{" "}
         <Link href={"/news-center"} className="text-[#cf932a]">
           Read More Here.
