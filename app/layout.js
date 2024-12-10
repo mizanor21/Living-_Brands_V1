31c2fd7cb3a2741777d8c3f3f1f3ac6cb427cf2a
mizan21 <mizanorrahman398@gmail.com>
2024-12-10 17:41:02 +0600

    Popup part polishing

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "../components/Shared/Nav/Nav";
 import Footer from "../components/Shared/Footer/Footer";
+import Popup from "@/components/Popup/Popup";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Popup />
         <Nav />
         {children}
         <Footer />
---
+++ components/Popup/Popup.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const Popup = () => {
+  return (
+    <div className="font-sora absolute flex justify-center items-center w-full z-[200] py-2">
+      <p className="font-[400] text-[18px]">
+        &quot;Addikt is now part of the Schbang Network.{" "}
+        <Link href={"/news-center"} className="text-[#cf932a]">
+          Read More Here.
+        </Link>
+        &quot;
+      </p>
+    </div>
+  );
+};
+
+export default Popup;
