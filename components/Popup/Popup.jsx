10041b43f5e781f5a6939adcfe0cb82538561aa2
mizan21 <mizanorrahman398@gmail.com>
2024-12-15 15:59:15 +0600

    popup confetti add

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import confetti from "@/public/assets/logo/confetti.gif";
 
 const Popup = () => {
   return (
     <div className="font-sora absolute flex justify-center items-center w-full z-[220] px-2 py-2">
+      <Image src={confetti} width={30} alt="confetti"></Image>
       <p className="font-[400] text-[13px] lg:text-[18px] text-center">
         &quot;Addikt is now part of the Living Brands Network.{" "}
         <Link href={"/news-center"} className="text-[#eba428]">
