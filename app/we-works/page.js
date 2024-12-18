1b0a5d6b0da264b36fcb1ab544988c87f3fd1e70
mizan21 <mizanorrahman398@gmail.com>
2024-12-18 16:41:22 +0600

    We works  adjustment

--- app/we-works/page.js
+++ app/we-works/page.js
@@ -6,7 +6,7 @@ const WeWork = () => {
     <div className="relative bg-white z-[110] rounded-b-[20px] lg:rounded-b-[60px]">
       <div className="px-[5%] ">
         <HowWeWorks></HowWeWorks>
-        <p className="font-sora font-[700] lg:text-[24px] py-10">
+        <p className="font-sora font-[700] text-[#125b5c] lg:text-[24px] py-10 md:py-14">
           Our Principles
         </p>
         <ColorPalette />
