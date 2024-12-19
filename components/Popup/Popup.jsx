c872c7e3fb67028e97673dd22180586cd9ae97f5
mizan21 <mizanorrahman398@gmail.com>
2024-12-19 22:00:14 +0600

    Adjust Popup component styles for improved layout and responsiveness

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -5,9 +5,9 @@ import confetti from "@/public/assets/logo/confetti.gif";
 
 const Popup = () => {
   return (
-    <div className="font-sora absolute flex justify-center items-center w-full z-[220] px-2 py-2">
+    <div className="font-sora md:sticky top-0 flex justify-center items-center w-full relative z-[220] px-2 py-2">
       <Image src={confetti} width={30} alt="confetti"></Image>
-      <p className="font-[400] text-[11px] lg:text-[18px] text-center">
+      <p className="font-[400] text-[0.7rem] lg:text-[1.13rem]">
         &quot;Addikt is now part of the Living Brands Network.{" "}
         <Link href={"/news-center"} className="text-[#ff6428]">
           Read More Here.
