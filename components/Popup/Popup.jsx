16f349ba704e46983102ad992a412d0c0194f565
mizan21 <mizanorrahman398@gmail.com>
2024-12-21 16:06:14 +0600

    Update Popup component styles; adjust font sizes for improved readability and add media query for larger screens

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -5,16 +5,22 @@ import confetti from "@/public/assets/logo/confetti.gif";
 
 const Popup = () => {
   return (
-    // font-sora absolute flex justify-center items-center w-full z-[220] px-2 py-2
     <div className="font-sora flex justify-center items-center w-full absolute px-2 py-2">
       <Image src={confetti} width={30} alt="confetti"></Image>
-      <p className="font-[400] text-[0.7rem] lg:text-[1rem]">
+      <p className="font-[400] text-[11px] md:text-[13px] lg:text-[14px] 2xl:text-[18px]">
         &quot;Addikt is now part of the Living Brands Network.{" "}
         <Link href={"/news-center"} className="text-[#ff6428]">
           Read More Here.
         </Link>
         &quot;
       </p>
+      <style jsx>{`
+        @media (min-width: 1700px) and (max-width: 2000px) {
+          p {
+            font-size: 18px;
+          }
+        }
+      `}</style>
     </div>
   );
 };
