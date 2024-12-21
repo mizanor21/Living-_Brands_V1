59371d4e0778af1af56cdcd988d1a8853e3fa54c
mizan21 <mizanorrahman398@gmail.com>
2024-12-21 16:08:45 +0600

    Update Popup component styles; adjust text size for improved readability on medium and large screens

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -7,7 +7,7 @@ const Popup = () => {
   return (
     <div className="font-sora flex justify-center items-center w-full absolute px-2 py-2">
       <Image src={confetti} width={30} alt="confetti"></Image>
-      <p className="font-[400] text-[11px] md:text-[13px] lg:text-[14px] 2xl:text-[18px]">
+      <p className="font-[400] text-[11px] md:text-[12px] lg:text-[13px] xl:text-[14px] 2xl:text-[18px]">
         &quot;Addikt is now part of the Living Brands Network.{" "}
         <Link href={"/news-center"} className="text-[#ff6428]">
           Read More Here.
