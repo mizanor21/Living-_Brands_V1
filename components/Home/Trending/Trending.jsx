65b57d21573c9172193a67a96064557c852c7d73
mizan21 <mizanorrahman398@gmail.com>
2024-12-21 11:22:18 +0600

    Update Trending component styles; remove bottom border radius for cleaner design Update Nav component styles; adjust side-menu visibility for better responsiveness

--- components/Home/Trending/Trending.jsx
+++ components/Home/Trending/Trending.jsx
@@ -3,7 +3,7 @@ import TrendingItems from "./TrendingItems";
 
 const Trending = () => {
   return (
-    <div className="relative z-[110] px-[5%] py-10 lg:pb-20 bg-white rounded-b-[20px] lg:rounded-b-[40px]">
+    <div className="relative z-[110] px-[5%] py-10 lg:pb-20 bg-white">
       <h2 className="font-sora font-[700] text-[20px] lg:text-[30px] pb-8">
         Trending Now
       </h2>
