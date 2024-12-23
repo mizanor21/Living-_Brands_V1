29e091c0c220e442875f29ed50a82376aa9a8403
mizan21 <mizanorrahman398@gmail.com>
2024-12-23 11:39:52 +0600

    Add motivational section to Careers page with core principles message

--- app/careers/page.js
+++ app/careers/page.js
@@ -9,6 +9,15 @@ const Career = () => {
       <div className="px-[5%] ">
         <CareerSection></CareerSection>
         <VideoSection></VideoSection>
+        <div className="text-center font-sora py-[40px] md:py-[45px] lg:py-[50px] xl:py-[60px] 2xl:py-[70]">
+          <h1 className="text-[#125b5c] text-[24px] font-[600] mt-0 lg:mt-10">
+            Have you got what it takes to #CreateALivingBrands?
+          </h1>
+          <p className="font-[400]  text-[19px] mt-2">
+            Check out our core principles. If they align with your values, do
+            apply..
+          </p>
+        </div>
         <ColorPalette />
         <CareerLastVideo></CareerLastVideo>
       </div>
