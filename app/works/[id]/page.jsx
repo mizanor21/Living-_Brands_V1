da6015ae5b9cf7608b3cb5fadd4e92304f83f3fc
mizan21 <mizanorrahman398@gmail.com>
2024-12-24 17:05:50 +0600

    Remove detailsTitle paragraph from WorkDetails component for cleaner layout

--- app/works/[id]/page.jsx
+++ app/works/[id]/page.jsx
@@ -20,7 +20,7 @@ const WorkDetails = ({ params }) => {
       <div className="w-full flex flex-col gap-3 text-[#125b5c] justify-start mt-16">
         <p className="text-[19px]">Case study</p>
         <h1 className="font-bold text-[24px] lg:text-[48px]">{title}</h1>
-        <p className="text-lg text-gray-600">{detailsTitle}</p>
+        {/* <p className="text-lg text-gray-600">{detailsTitle}</p> */}
       </div>
 
       {/* Image Section */}
