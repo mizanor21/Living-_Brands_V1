1cf89b90985993c9307e0fccdd65a016d0e8fda4
mizan21 <mizanorrahman398@gmail.com>
2024-12-25 09:58:43 +0600

    Refactor Blogs component layout for improved spacing and consistency

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -4,8 +4,8 @@ import BlogItems from "../blog-items/page";
 
 const Blogs = () => {
   return (
-    <div className="font-sora px-[5%] mb-10 lg:mb-20 bg-white relative z-[110] rounded-b-[20px] lg:rounded-b-[60px] pb-5 lg:pb-20 ">
-      <h1 className="text-2xl lg:text-[48px] font-bold text-[#135c58] text-center my-10 lg:my-20">
+    <div className="font-sora px-[5%] bg-white relative z-[110] rounded-b-[20px] lg:rounded-b-[40px] py-2 lg:pb-10 xl:pb-14 2xl:pb-20">
+      <h1 className="text-2xl lg:text-[48px] font-bold text-[#135c58] text-center mb-10 lg:my-20">
         Blogs
       </h1>
       <BlogItems />
