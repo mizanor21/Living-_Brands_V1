79825e0873510235950c5e13a71772dfd4c4a20d
mizan21 <mizanorrahman398@gmail.com>
2024-12-26 11:09:03 +0600

    Fix padding issue in Trending component for improved layout

--- components/Home/Trending/Trending.jsx
+++ components/Home/Trending/Trending.jsx
@@ -3,7 +3,7 @@ import TrendingItems from "./TrendingItems";
 
 const Trending = () => {
   return (
-    <div className="relative z-[110] px-[5%] py-10 lg:pb-20 bg-white">
+    <div className="relative z-[110] px-[5%] mb:py-10 lg:pb-20 bg-white">
       <h2 className="font-sora font-[700] text-[30px] pb-8">Trending Now</h2>
       <TrendingItems />
     </div>
