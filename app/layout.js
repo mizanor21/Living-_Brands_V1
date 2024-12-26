7d61466460b9be42ed7883820686fbdb9fef2129
mizan21 <mizanorrahman398@gmail.com>
2024-12-26 17:51:21 +0600

    Update scrollbar styling and adjust layout structure for improved user experience

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} scrollbar`}>
+    <html lang="en" className="scrollbar">
+      <body className={`${inter.className}`}>
         <Nav />
         {children}
         <Footer />
