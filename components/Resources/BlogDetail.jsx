27909ffbace5c7ee2b61001212031d57e30ef4f7
mizan21 <mizanorrahman398@gmail.com>
2024-12-30 12:10:39 +0600

    Remove loading component and blog details page; add Facebook sharing functionality in BlogDetail component

--- components/Resources/BlogDetail.jsx
+++ components/Resources/BlogDetail.jsx
@@ -2,6 +2,27 @@ import Link from "next/link";
 import React from "react";
 
 const BlogDetail = ({ data }) => {
+  console.log(data.blog._id);
+  const handleShare = () => {
+    try {
+      // Use the simpler sharer.php approach which is more reliable
+      const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+        `https://living-brands-v1.vercel.app/blogs/${data.blog._id}`
+      )}`;
+      const width = 600;
+      const height = 400;
+      const left = window.screen.width / 2 - width / 2;
+      const top = window.screen.height / 2 - height / 2;
+
+      window.open(
+        shareUrl,
+        "facebook-share-dialog",
+        `width=${width},height=${height},left=${left},top=${top},toolbar=0,status=0,menubar=0`
+      );
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  };
   return (
     <div className="font-sora px-[5%]">
       <div className="lg:px-[10%]">
@@ -48,8 +69,9 @@ const BlogDetail = ({ data }) => {
                     <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path>
                   </svg>
                 </Link>
-                <Link
-                  href={"https://www.facebook.com/Livingbrands.co"}
+                <button
+                  // href={"https://www.facebook.com/Livingbrands.co"}
+                  onClick={handleShare}
                   target="_blank"
                   className="flex items-center justify-center text-black bg-[#F4F4F4] bg-transparent border border-[#F4F4F4] rounded-full w-7 h-7"
                 >
@@ -61,7 +83,7 @@ const BlogDetail = ({ data }) => {
                   >
                     <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
                   </svg>
-                </Link>
+                </button>
               </div>
             </div>
 
@@ -207,7 +229,7 @@ const BlogDetail = ({ data }) => {
                   </ul>
                 </ul>
               </ul>
-              <p className="text-[#125b5c] text-2xl text-2xl md:text-4xl font-bold py-5">
+              <p className="text-[#125b5c] text-2xl md:text-4xl font-bold py-5">
                 Making Hindustan Coca-Cola Beverages Great Again
               </p>
 
