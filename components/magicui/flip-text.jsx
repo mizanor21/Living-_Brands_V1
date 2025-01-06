6607b870008db97d5352c27ca5f9a5df24017513
mizan21 <mizanorrahman398@gmail.com>
2025-01-06 11:15:38 +0600

    testing

--- components/magicui/flip-text.jsx
+++ components/magicui/flip-text.jsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 
 export default function SlightFlip({
   word = "", // Default to an empty string to prevent errors
-  duration = 0,
-  delayMultiple = 0,
+  duration = 1,
+  delayMultiple = 0.01,
   framerProps = {
     hidden: { rotateX: -90, opacity: 0 },
     visible: { rotateX: 0, opacity: 1 },
@@ -24,8 +24,8 @@ export default function SlightFlip({
         if (entry.isIntersecting) {
           setIsVisible(true); // Set visible when the component enters the viewport
         }
-      }
-      // { threshold: 0.1 } // Trigger when at least 10% of the component is visible
+      },
+      { threshold: 0.1 } // Trigger when at least 10% of the component is visible
     );
 
     if (ref.current) {
